4a259cee8ef266a528cae0e3e4d1c25ac86478dd
bituan <113036994+bituann@users.noreply.github.com>
2025-10-17 00:33:18 +0100

    Modify controller to use user service class

--- src/main/java/com/bituan/simple_api/controller/SimpleApiController.java
+++ src/main/java/com/bituan/simple_api/controller/SimpleApiController.java
@@ -1,8 +1,22 @@
 package com.bituan.simple_api.controller;
 
+import com.bituan.simple_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleApiController {
 
+    private final UserService userService;
+
+    @Autowired
+    public SimpleApiController (UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public String getResponse () {
+        return userService.getUserAsJSONString();
+    }
 }
