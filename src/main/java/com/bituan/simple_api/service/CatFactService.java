85c5c0c630c7c9aba8ac70e977809e2168d86a02
bituan <113036994+bituann@users.noreply.github.com>
2025-10-17 10:07:51 +0100

    Remove unused import

--- src/main/java/com/bituan/simple_api/service/CatFactService.java
+++ src/main/java/com/bituan/simple_api/service/CatFactService.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Objects;
-
 @Service
 public class CatFactService {
 
