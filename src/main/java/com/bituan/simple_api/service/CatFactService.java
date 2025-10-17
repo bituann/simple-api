b42b5d78ade719f962d4c7917123d7615d97f53f
bituan <113036994+bituann@users.noreply.github.com>
2025-10-17 10:07:07 +0100

    Complete cat fact service plus error handling

--- src/main/java/com/bituan/simple_api/service/CatFactService.java
+++ src/main/java/com/bituan/simple_api/service/CatFactService.java
@@ -1,14 +1,28 @@
 package com.bituan.simple_api.service;
 
+import com.bituan.simple_api.model.CatFact;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 public class CatFactService {
 
-    private final String catFactApiUri = "https://catfact.ninja/fact";
-
     public String getCatFact () {
-        return new RestTemplate().getForObject(catFactApiUri, String.class);
+        RestTemplate restTemplate = new RestTemplate();
+        final String catFactApiUri = "https://catfact.ninja/fact";
+
+        ResponseEntity<CatFact> response = restTemplate.getForEntity(catFactApiUri, CatFact.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody().getFact();
+        } else {
+            return "Sometimes, there are simply no facts! (Our facts provider is down)";
+        }
+
+
     }
 }
