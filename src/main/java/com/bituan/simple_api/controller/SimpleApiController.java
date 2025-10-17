9e0c0bb124da1d8377a8736d9483dd11d7335248
bituan <113036994+bituann@users.noreply.github.com>
2025-10-17 08:58:36 +0100

    Use proper response class; integrate cat fact service

--- src/main/java/com/bituan/simple_api/controller/SimpleApiController.java
+++ src/main/java/com/bituan/simple_api/controller/SimpleApiController.java
@@ -1,7 +1,9 @@
 package com.bituan.simple_api.controller;
 
+import com.bituan.simple_api.service.CatFactService;
 import com.bituan.simple_api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleApiController {
 
     private final UserService userService;
+    private final CatFactService catFactService;
 
     @Autowired
-    public SimpleApiController (UserService userService) {
+    public SimpleApiController (UserService userService, CatFactService catFactService) {
         this.userService = userService;
+        this.catFactService = catFactService;
     }
 
     @GetMapping("/me")
-    public String getResponse () {
-        return userService.getUserAsJSONString();
+    public ResponseEntity<String> getResponse () {
+        return ResponseEntity
+                .ok()
+                .header("Content-Type", "application/json")
+                .body("%s\n%s".formatted(userService.getUserAsJSONString(), catFactService.getCatFact()));
     }
 }
