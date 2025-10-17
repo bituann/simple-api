251ed03f8a51c839bfb4e190ec4f7d4c9021e761
bituan <113036994+bituann@users.noreply.github.com>
2025-10-17 09:14:50 +0100

    Add cat fact data model

---
+++ src/main/java/com/bituan/simple_api/model/CatFact.java
@@ -0,0 +1,27 @@
+package com.bituan.simple_api.model;
+
+public class CatFact {
+    private String fact;
+    private int length;
+
+    public CatFact(String fact, int length) {
+        this.fact = fact;
+        this.length = length;
+    }
+
+    public String getFact() {
+        return fact;
+    }
+
+    public void setFact(String fact) {
+        this.fact = fact;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
